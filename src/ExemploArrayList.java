f34315c2f60779e4442e78fbb2969deb38f6bbe2
codelize <lflizier@gmail.com>
2024-11-25 22:48:40 -0300

    1. Refaça os exercícios de vetores, agora utilizando somente ArrayList e/ou HashSet no lugar do vetor

---
+++ src/ExemploArrayList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class ExemploArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> alunos = new ArrayList<>();
+
+        // Adicionando elementos
+        alunos.add("Ana");
+        alunos.add("Bruno");
+        alunos.add("Carlos");
+
+        // Exibindo os elementos
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
+}
---
+++ src/ExemploHashSet.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+public class ExemploHashSet {
+    public static void main(String[] args) {
+        HashSet<String> alunos = new HashSet<>();
+
+        // Adicionando elementos
+        alunos.add("Ana");
+        alunos.add("Bruno");
+        alunos.add("Carlos");
+
+        // Exibindo os elementos
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
+}
